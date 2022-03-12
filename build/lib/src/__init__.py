b75cb374f85577e0cdc7281a4e38308f3f799e86
zhaochangming <1730514926@qq.com>
2022-03-12 19:39:55 +0800

    test

--- build/lib/src/__init__.py
+++
@@ -1,4 +0,0 @@
-from .MVBLS import MVBLSRegressor, MVBLSClassifier, SemiMVBLSRegressor, SemiMVBLSClassifier
-from ._version import __version__
-
-__all__ = ['MVBLSRegressor', 'MVBLSClassifier', 'SemiMVBLSRegressor', 'SemiMVBLSClassifier', '__version__']

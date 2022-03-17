c4da5b2331ebb2da2f0cdb7d3988f4ea9f693012
zhaochangming <1730514926@qq.com>
2022-03-17 16:25:29 +0800

    cmzhao

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: Apache",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',  # 对python的最低版本要求
